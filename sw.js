d23b77259bdca112445d4fd0c6078f0932828646
Bryan Woo <94886394+bryanwoo988@users.noreply.github.com>
2026-08-05 12:36:50 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wordslip-v3';
+const CACHE = 'memocab-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './apple-touch-icon.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
